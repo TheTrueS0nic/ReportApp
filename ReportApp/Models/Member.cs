ce0ae786e25b99acf345f959d55133c92bb22885
Dustin Dyckmanns <dustin.dyckmanns@cgi.com>
2023-08-28 16:17:03 +0200

    Display Namen hinzugefügt für Member

--- ReportApp/Models/Member.cs
+++ ReportApp/Models/Member.cs
@@ -1,11 +1,16 @@
-﻿namespace ReportApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace ReportApp.Models
 {
     public class Member
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public string Signature { get; set; }
+        public required string Signature { get; set; }
+        [Display(Name = "Joined at")]
         public DateTime JoinedAt { get; set; }
+        [Display(Name = "Is a Trainee")]
         public bool IsTrainee { get; set; }
 
     }
