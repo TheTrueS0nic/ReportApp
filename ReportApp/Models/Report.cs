560cf043a6c7c7b9ad26b54f9bebe99154948e87
Dustin Dyckmanns <dustin.dyckmanns@cgi.com>
2023-09-01 14:04:55 +0200

    Report view bearbeitet

--- ReportApp/Models/Report.cs
+++ ReportApp/Models/Report.cs
@@ -1,11 +1,16 @@
-﻿namespace ReportApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReportApp.Models
 {
     public class Report
     {
         public int Id { get; set; }
+        [Display(Name = "Trainee")]
         public int OriginatorId { get; set; }
+        [Display(Name = "Trainer")]
         public int ReviewerId { get; set; }
         public required string Content { get; set; }
+        [Display(Name = "Creation Date")]
         public DateTime CreationDate { get; set; }
         public DateTime? ModificationDate { get; set;}
         public DateTime? ApprovalDate { get; set; }
