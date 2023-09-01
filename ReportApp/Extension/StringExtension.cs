c239f09f439add4ad73a4b3cf3674ea842d5a99f
Dustin Dyckmanns <dustin.dyckmanns@cgi.com>
2023-09-01 12:08:32 +0200

    StringExtension gebaut

---
+++ ReportApp/Extension/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace ReportApp.Extension
+{
+    public static class StringExtention
+    {
+        public static string ContentPreview(this string text)
+        {
+            if (text.Length > 20)
+            {
+                text = text.Substring(0, 20);
+                return text;
+            }
+            return text;
+        }
+    }
+}
